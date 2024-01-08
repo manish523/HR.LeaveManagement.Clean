b625a9ed5fbd6675a903069225049c458f34ee45
Manish Kumar <manish.callme@gmail.com>
2024-01-08 23:41:05 +0530

    Add project files.

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypeDetails;
+
+public class GetLeaveTypeDetailsQueryHandler : IRequestHandler<LeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public GetLeaveTypeDetailsQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+    public async Task<LeaveTypeDetailsDto> Handle(LeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+    {
+        // query the database
+        var leaveType = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+        // verify that record exists
+        if (leaveType == null)
+            throw new NotFoundException(nameof(LeaveType), request.Id);
+
+        // convert data objects to DTO objects
+        var data = _mapper.Map<LeaveTypeDetailsDto>(leaveType);
+
+        // return list of DTO object
+        return data;
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypeDetails/LeaveTypeDetailsQuery.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypeDetails;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypeDetails
+{
+    public record LeaveTypeDetailsQuery(int Id) : IRequest<LeaveTypeDetailsDto>;
+}
