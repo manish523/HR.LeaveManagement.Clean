5315a0bcd7077fc358e99098ab10866d1ea84120
Manish Kumar <manish.callme@gmail.com>
2024-01-10 01:32:49 +0530

    setup persistence project with ef core & implemented repositories. Added Infrastructure project for third party services like email and option pattern. Also implemeted Logging services

---
+++ HR.LeaveManagement.Persistence/Configurations/LeaveTypeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Configurations
+{
+    public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+    {
+        public void Configure(EntityTypeBuilder<LeaveType> builder)
+        {
+            builder.HasData(
+                new LeaveType
+                {
+                    Id = 1,
+                    Name = "Vacation",
+                    DefaultDays = 10,
+                    DateCreated = DateTime.Now,
+                    DateModified = DateTime.Now,
+                }
+            );
+
+            // We can add some rules for our fields here
+            // While fluentvalidations works at model level, this works at database level.
+            builder.Property(q => q.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -0,0 +1,66 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using HR.LeaveManagement.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.DatabaseContext;
+
+public class HrDatabaseContext : DbContext
+{
+    public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Different ways of applying entity type configuration
+
+        // 1- This will look for all configurations and will apply them automatically
+        //modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
+
+        // 2- This is the way where we need to register all separately written configurations one by one
+        //modelBuilder.ApplyConfiguration(new LeaveTypeConfiguration());
+
+        // 3- We don't need this type of configuration all at once.
+        //    we have to write configuration for all entities here 1 by 1
+        //    We don't need it if we are writing configurations in separate files like we are doing for this project
+        //modelBuilder.Entity<LeaveType>().HasData(
+        //    new LeaveType
+        //    {
+        //        Id = 1,
+        //        Name = "Vacation",
+        //        DefaultDays = 10,
+        //        DateCreated = DateTime.Now,
+        //        DateModified = DateTime.Now,
+        //    }
+        //);
+
+        // This when runs looks for all configurations be it of any type and executes them
+        //base.OnModelCreating(modelBuilder);
+
+        // So this is the most cleaner and preferred way of doing it and seed data properly
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+        {
+            entry.Entity.DateModified = DateTime.Now;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
